b85c4b659c5c31701564b36cd6a1e7390a61650b
Severin Gsponer <svgsponer@gmail.com>
2020-05-27 16:27:10 +0200

    Change style

--- app.py
+++ app.py
@@ -24,16 +24,32 @@ demo_kernel = np.array([1, 2, 1])
 
 
 @app.callback(
+    [Output(component_id='test-ts-container', component_property='style'),
+     Output(component_id='ts-select-container', component_property='style'),],
+    [Input(component_id='test_ts_switch', component_property='on'),],)
+
+def update_test_ts_style(use_test_ts):
+    if not use_test_ts:
+        test_ts_style = {'display': 'none'}
+        slider_style = {}
+    else:
+        test_ts_style = {}
+        slider_style = {'display': 'none'}
+    return test_ts_style, slider_style
+
+
+@app.callback(
     Output(component_id='current-TS', component_property='children'),
     [Input(component_id='ts-select', component_property='value'),
-     Input(component_id='ts-chkb', component_property='value'),
+     Input(component_id='normalize_ts_switch', component_property='on'),
+     Input(component_id='test_ts_switch', component_property='on'),
      Input(component_id='test-ts', component_property='value'),
      Input(component_id='all-data', component_property='children'),
      ]
 )
-def prepare_ts(ts_idx, chk_list, test_ts, data_json):
+def prepare_ts(ts_idx, do_normalize, do_use_test_ts, test_ts, data_json):
     print('Prepare TS')
-    if 'test' in chk_list:
+    if do_use_test_ts:
         ts = np.array(list(map(float, test_ts.split(','))))
         ts = pd.DataFrame(ts)
     else:
@@ -41,7 +57,7 @@ def prepare_ts(ts_idx, chk_list, test_ts, data_json):
         ts_idx = int(ts_idx)
         ts = data[ts_idx]
 
-    if 'norm' in chk_list:
+    if do_normalize:
         ts = utls.noramlize(ts)
 
     return ts.to_json(orient='values')
@@ -142,7 +158,7 @@ app.layout = html.Div(children=[
             children=html.Div([
                 'Drag and Drop or ',
                 html.A('Select Files'),
-                html.P('Note the expected format is a csv file, each time series on a seperate line and with the first entry beeing the class label (will be omitted).',)
+                html.P('Note, the expected format is a csv file, each time series on a seperate line and with the first entry beeing the class label (will be omitted).',)
             ]),
             style={
                 'width': '100%',
@@ -163,33 +179,50 @@ app.layout = html.Div(children=[
     html.Div([
         html.Div([
             html.Div(children=[
-                dcc.Checklist(
-                    options=[
-                        {'label': 'Normalize', 'value': 'norm'},
-                        {'label': 'Use test time series', 'value': 'test'},
-                    ],
-                    value=[],
-                    id='ts-chkb'
-                )]),
+                daq.BooleanSwitch(
+                    id="normalize_ts_switch",
+                    on=False,
+                    label='Normalize time series',
+                ),
+                daq.BooleanSwitch(
+                    id="test_ts_switch",
+                    on=False,
+                    label='Use test time series',
+                ),
+            ]),
 
             # Test time series
-            html.Div(children=[
-                dcc.Input(
-                    id="test-ts",
-                    placeholder='Enter your test time series',
-                    type='text',
-                    value='1,1,1,1,1,1,1,1,1,1,1,1,1',
-                    debounce=True
-                )])],
-                 className="two columns"),
+
+            html.Div([  # div to keep spacing correct
+                html.Div(
+                    id="test-ts-container",
+                    children=[
+                        dcc.Input(
+                            id="test-ts",
+                            placeholder='Enter your test time series',
+                            type='text',
+                            value='1,1,1,1,1,1,1,1,1,1,1,1,1',
+                            debounce=True
+                        )],),
+
+                html.Div(
+                    id="ts-select-container",
+                    children=[
+                        html.P(["Select time series"],
+                               style={
+                                   'width': '100%',
+                                   'textAlign': 'center'}),
+                        dcc.Slider(
+                            id='ts-select',
+                            min=-0,
+                            step=1,
+                            value=0),
+                    ]),
+            ],
+                     style={'margin-top': '1em'},)
+        ], className="two columns"),
+
         html.Div([
-            html.H6(["Select time series"]),
-            dcc.Slider(
-                id='ts-select',
-                min=-0,
-                step=1,
-                value=0
-            ),
             dcc.Graph(
                 id="ts_plot"),
         ], className="ten columns"),
@@ -212,10 +245,6 @@ app.layout = html.Div(children=[
                     on=False,
                     label='Center Kernel',
                     style={'alignItems': 'flex-star'}
-                    # options=[
-                    #     {'label': 'Center kernel', 'value': 'center'}
-                    #     ],
-                    # value=[],
                 ),
             ]),
             # Kernel bias
