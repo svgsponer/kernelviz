5227c5ddd80b5867da8dada680a927fbc1d93477
Severin Gsponer <svgsponer@gmail.com>
2020-05-27 15:08:22 +0200

    add upload of custom datasets

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
+import dash_daq as daq
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import pandas as pd
 import numpy as np
 import plotly.graph_objs as go
 import json
+import base64
+import io
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-datafile='data/CMJ/JumpResampled/JumpResampled_TRAIN'
-#datafile='../data/1M-TSC-SITS_2006_NDVI_C/SITS1M_fold1/SITS1M_fold1_TRAIN.csv'
-data = pd.DataFrame(np.loadtxt(datafile, delimiter=',')[:, 1:].T)
-print(data.shape)
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title = "KernelViz"
 demo_kernel = np.array([1, 2, 1])
+
 
 def add_dila_to_kernel(weights, dilation):
     kernel_length = len(weights) + (len(weights) - 1) * dilation
@@ -35,6 +35,7 @@ def add_dila_to_kernel(weights, dilation):
             sup += 1
     return kernel
 
+
 def apply_kernel(ts, kernel, bias, padding, stride):
 
     # if padding > 0:
@@ -45,7 +46,6 @@ def apply_kernel(ts, kernel, bias, padding, stride):
 
     # Add padding to kernel
     # kernel = add_dila_to_kernel(weights, dilation)
-    print(kernel)
 
     kernel_length = kernel.shape[0]
 
@@ -58,16 +58,10 @@ def apply_kernel(ts, kernel, bias, padding, stride):
     for i in range(0, output_length):
         _sum = bias
         for j in range(0, kernel_length, stride):
-            print(kernel[j])
             s = kernel[j] * ts[i + j]
-            print(s)
             _sum += s
             output[i] = _sum
 
-    print(output_length)
-    print(input_length)
-    print(kernel_length)
-    print(output)
     return output
 
 
@@ -78,16 +72,19 @@ def noramlize(ts):
 
 @app.callback(
     Output(component_id='current-TS', component_property='children'),
-    [Input(component_id='ts_select', component_property='value'),
+    [Input(component_id='ts-select', component_property='value'),
      Input(component_id='ts-chkb', component_property='value'),
      Input(component_id='test-ts', component_property='value'),
+     Input(component_id='all-data', component_property='children'),
      ]
 )
-def prepare_ts(ts_idx, chk_list, test_ts):
+def prepare_ts(ts_idx, chk_list, test_ts, data_json):
+    print('Prepare TS')
     if 'test' in chk_list:
         ts = np.array(list(map(float, test_ts.split(','))))
         ts = pd.DataFrame(ts)
     else:
+        data = pd.read_json(data_json, orient='values')
         ts_idx = int(ts_idx)
         ts = data[ts_idx]
 
@@ -102,8 +99,8 @@ def prepare_ts(ts_idx, chk_list, test_ts):
     [Input(component_id='current-TS', component_property='children')]
 )
 def plot_ts(json_data):
+    print('Plot TS')
     dff = pd.read_json(json_data, orient='values').to_numpy().flatten()
-    print(dff)
     layout = {'title': {'text': "Time series"}}
     return go.Figure(data=[go.Scatter(y=dff)], layout=layout)
 
@@ -111,18 +108,18 @@ def plot_ts(json_data):
 @app.callback(
     Output(component_id='current-kernel', component_property='children'),
     [Input(component_id='kernel_weights', component_property='value'),
-     Input(component_id='kernel_centering_chkb', component_property='value'),
+     Input(component_id='kernel_centering_chkb', component_property='on'),
      Input(component_id='kernel_dilation', component_property='value'),
      ])
 def prepare_kernel(kernel, kc_ckhkb, dilation):
     dilation = int(dilation)
     kernel = np.array(list(map(float, kernel.split(','))))
-    if 'center' in kc_ckhkb:
+    if kc_ckhkb:
         mean = np.mean(kernel)
         kernel = kernel - mean
     dila_kernel = add_dila_to_kernel(kernel, dilation)
-    ret ={'original_kernel': kernel.tolist(),
-          'dila_kernel': dila_kernel.tolist()}
+    ret = {'original_kernel': kernel.tolist(),
+           'dila_kernel': dila_kernel.tolist()}
     return json.dumps(ret)
 
 
@@ -151,7 +148,6 @@ def plot_trans_ts(json_data, kernel, bias, padding, stride):
 
     dff = pd.read_json(json_data, orient='values').to_numpy().flatten()
     kernel = np.array(json.loads(kernel)['dila_kernel'])
-    print(kernel)
     bias = float(bias)
     padding = int(padding)
     stride = int(stride)
@@ -160,12 +156,56 @@ def plot_trans_ts(json_data, kernel, bias, padding, stride):
     return go.Figure(data=[go.Scatter(y=transformed)], layout=layout)
 
 
+@app.callback(
+    [Output('all-data', 'children'),
+     Output('ts-select', 'max'),
+     Output('ts-select', 'value'),],
+    [Input('upload-data', 'contents')],)
+def upload_data(list_of_contents):
+    if list_of_contents is not None:
+        print("Load uploaded dataset")
+        content_type, content_string = list_of_contents.split(',')
+        decoded = base64.b64decode(content_string)
+        c = io.BytesIO(decoded)
+        data = pd.DataFrame(np.loadtxt(c, delimiter=',')[:, 1:].T)
+    else:
+        print("Load default dataset")
+        datafile='data/CMJ/JumpResampled/JumpResampled_TRAIN'
+        #datafile='../data/1M-TSC-SITS_2006_NDVI_C/SITS1M_fold1/SITS1M_fold1_TRAIN.csv'
+        data = pd.DataFrame(np.loadtxt(datafile, delimiter=',')[:, 1:].T)
+    print(data.shape)
+    return data.to_json(orient='values'), data.shape[1]-1, 0
+
+
+
 app.layout = html.Div(children=[
     html.H4(children='TS kernel visualization'),
     # Div to store the current TS
     html.Div(id='current-TS', style={'display': 'none'}),
     html.Div(id='current-kernel', style={'display': 'none'}),
-
+    html.Div([
+        dcc.Upload(
+            id='upload-data',
+            children=html.Div([
+                'Drag and Drop or ',
+                html.A('Select Files'),
+                html.P('Note the expected format is a csv file, each time series on a seperate line and with the first entry beeing the class label (will be omitted).',)
+            ]),
+            style={
+                'width': '100%',
+                'height': '120px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px'
+            },
+            # Allow multiple files to be uploaded
+        multiple=False
+        ),
+        html.Div(id='all-data', style={'display': 'none'}),
+    ]),
     # Time series plot
     html.Div([
         html.Div([
@@ -188,13 +228,12 @@ app.layout = html.Div(children=[
                     value='1,1,1,1,1,1,1,1,1,1,1,1,1',
                     debounce=True
                 )])],
-            className="two columns"),
+                 className="two columns"),
         html.Div([
             html.H6(["Select time series"]),
             dcc.Slider(
-                id='ts_select',
+                id='ts-select',
                 min=-0,
-                max=data.shape[0],
                 step=1,
                 value=0
             ),
@@ -215,12 +254,15 @@ app.layout = html.Div(children=[
                     value='1,2,1',
                     debounce=True
                 ),
-                dcc.Checklist(
+                daq.BooleanSwitch(
                     id="kernel_centering_chkb",
-                    options=[
-                        {'label': 'Center kernel', 'value': 'center'}
-                        ],
-                    value=[],
+                    on=False,
+                    label='Center Kernel',
+                    style={'alignItems': 'flex-star'}
+                    # options=[
+                    #     {'label': 'Center kernel', 'value': 'center'}
+                    #     ],
+                    # value=[],
                 ),
             ]),
             # Kernel bias
